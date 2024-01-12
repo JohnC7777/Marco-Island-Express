ac73edcc677ab235d8157b9722b4ed74d6beb537
John Citrowske <john.citrowske@gmail.com>
2024-01-11 18:51:13 -0600

    Initial Commit

---
+++ Marco Island Express/Marco_Island_ExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marco_Island_ExpressApp.swift
+//  Marco Island Express
+//
+//  Created by United States MO on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Marco_Island_ExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
