4d040449a560a640a6bb1d8d3f5d1acb3c59284c
John Citrowske <john.citrowske@gmail.com>
2024-01-26 22:44:48 -0600

    Added airport airline and flight number selection for pickup. Refined Details sheet

---
+++ Marco Island Express/Core/Booking/Details/Sheet/DetailsSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsSheetView.swift
+//  Marco Island Express
+//
+//  Created by United States MO on 1/24/24.
+//
+
+import SwiftUI
+
+struct DetailsSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsSheetView()
+}
---
+++ Marco Island Express/Core/Booking/Payment/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  Marco Island Express
+//
+//  Created by United States MO on 1/24/24.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
